bf2eafcd3403168daab81169114d4941a0720959
Danilo Zabala II <dczii@live.com>
2024-12-02 18:52:16 +0800

    Add preloader.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import classNames from "classnames";
 import Link from "next/link";
 import Image from "next/image";
 import { useScroll, useSpring, motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
 
-import { AnimatedMaskLogo, Footer, WorkList } from "@z/components";
+import { AnimatedMaskLogo, Footer, Preloader, WorkList } from "@z/components";
 
 export type WorkDataType = {
   year: string;
@@ -38,40 +39,77 @@ export default function Home() {
   };
 
   return (
-    <div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 flex flex-col max-w-[1440px] mx-auto'>
-      <main className='flex flex-col items-center w-screen'>
-        <div className='sticky top-0 z-10 w-full py-10'>
-          <AnimatedMaskLogo />
-        </div>
+    <>
+      <AnimatePresence mode='wait'>
+        <Preloader />
+      </AnimatePresence>
+      <div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 flex flex-col max-w-[1440px] mx-auto'>
+        <main className='flex flex-col items-center w-screen'>
+          <div className='sticky top-0 z-10 w-full py-10'>
+            <AnimatedMaskLogo />
+          </div>
 
-        <div className='welcome-container '>
-          <div className='grid grid-cols-2 items-center'>
-            <motion.div
-              className='text-xl font-medium text-[#d6d6d6] col-span-2 md:col-span-1'
-              variants={container}
-              initial='hidden'
-              animate='visible'
-            >
-              {introduction.split(" ").map((word, index) => (
-                <motion.span
-                  key={index}
-                  className={classNames("inline-block mr-1", {
-                    "font-bold": index <= 5,
-                    "font-light": index > 5,
-                  })}
-                  variants={wordAnimation}
-                >
-                  {word}
-                </motion.span>
-              ))}
-              <Link href='/3d'>3D Page</Link>
-            </motion.div>
-
-            <div className='col-span-2 md:col-span-1 flex justify-end'>
+          <div className='welcome-container '>
+            <div className='grid grid-cols-2 items-center'>
               <motion.div
+                className='text-xl font-medium text-[#d6d6d6] col-span-2 md:col-span-1'
+                variants={container}
+                initial='hidden'
+                animate='visible'
+              >
+                {introduction.split(" ").map((word, index) => (
+                  <motion.span
+                    key={index}
+                    className={classNames("inline-block mr-1", {
+                      "font-bold": index <= 5,
+                      "font-light": index > 5,
+                    })}
+                    variants={wordAnimation}
+                  >
+                    {word}
+                  </motion.span>
+                ))}
+                <Link href='/3d'>3D Page</Link>
+              </motion.div>
+
+              <div className='col-span-2 md:col-span-1 flex justify-end'>
+                <motion.div
+                  animate={{
+                    opacity: 1,
+                    y: [0, -50, 0], // Bounce effect after fade-in
+                  }}
+                  variants={{
+                    hidden: { opacity: 0, y: 50 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  transition={{
+                    opacity: {
+                      duration: 2, // Duration for fade-in
+                      delay: 3, // Delay fade-in by 5 seconds
+                    },
+                    y: {
+                      duration: 3, // Bounce duration
+                      repeat: Infinity, // Repeat bouncing
+                      ease: "easeInOut", // Smooth bounce
+                    },
+                  }}
+                >
+                  <Image
+                    src='/astronaut.png'
+                    alt='logo'
+                    className='z-10'
+                    height={600}
+                    width={600}
+                  />
+                </motion.div>
+              </div>
+            </div>
+            <div className='absolute bottom-[15vh]'>
+              <motion.div
+                className='h-[26px] w-[19px]'
                 animate={{
                   opacity: 1,
-                  y: [0, -50, 0], // Bounce effect after fade-in
+                  y: [0, -10, 0], // Bounce effect after fade-in
                 }}
                 variants={{
                   hidden: { opacity: 0, y: 50 },
@@ -89,65 +127,39 @@ export default function Home() {
                   },
                 }}
               >
-                <Image src='/astronaut.png' alt='logo' className='z-10' height={600} width={600} />
+                <svg viewBox='0 0 19 26' id='svg12111482903' className='rotate-90'>
+                  <path
+                    d='M 15 11 C 16.105 11 17 11.895 17 13 C 17 14.105 16.105 15 15 15 C 13.895 15 13 14.105 13 13 C 13 11.895 13.895 11 15 11 Z'
+                    fill='#FFF'
+                  ></path>
+                  <path
+                    d='M 5 1 C 6.105 1 7 1.895 7 3 C 7 4.105 6.105 5 5 5 C 3.895 5 3 4.105 3 3 C 3 1.895 3.895 1 5 1 Z'
+                    fill='#FFF'
+                  ></path>
+                  <path
+                    d='M 10 6 C 11.105 6 12 6.895 12 8 C 12 9.105 11.105 10 10 10 C 8.895 10 8 9.105 8 8 C 8 6.895 8.895 6 10 6 Z'
+                    fill='#FFF'
+                  ></path>
+                  <path
+                    d='M 5 21 C 6.105 21 7 21.895 7 23 C 7 24.105 6.105 25 5 25 C 3.895 25 3 24.105 3 23 C 3 21.895 3.895 21 5 21 Z'
+                    fill='#FFF'
+                  ></path>
+                  <path
+                    d='M 10 16 C 11.105 16 12 16.895 12 18 C 12 19.105 11.105 20 10 20 C 8.895 20 8 19.105 8 18 C 8 16.895 8.895 16 10 16 Z'
+                    fill='#FFF'
+                  ></path>
+                </svg>
               </motion.div>
             </div>
           </div>
-          <div className='absolute bottom-[15vh]'>
-            <motion.div
-              className='h-[26px] w-[19px]'
-              animate={{
-                opacity: 1,
-                y: [0, -10, 0], // Bounce effect after fade-in
-              }}
-              variants={{
-                hidden: { opacity: 0, y: 50 },
-                visible: { opacity: 1, y: 0 },
-              }}
-              transition={{
-                opacity: {
-                  duration: 2, // Duration for fade-in
-                  delay: 3, // Delay fade-in by 5 seconds
-                },
-                y: {
-                  duration: 3, // Bounce duration
-                  repeat: Infinity, // Repeat bouncing
-                  ease: "easeInOut", // Smooth bounce
-                },
-              }}
-            >
-              <svg viewBox='0 0 19 26' id='svg12111482903' className='rotate-90'>
-                <path
-                  d='M 15 11 C 16.105 11 17 11.895 17 13 C 17 14.105 16.105 15 15 15 C 13.895 15 13 14.105 13 13 C 13 11.895 13.895 11 15 11 Z'
-                  fill='#FFF'
-                ></path>
-                <path
-                  d='M 5 1 C 6.105 1 7 1.895 7 3 C 7 4.105 6.105 5 5 5 C 3.895 5 3 4.105 3 3 C 3 1.895 3.895 1 5 1 Z'
-                  fill='#FFF'
-                ></path>
-                <path
-                  d='M 10 6 C 11.105 6 12 6.895 12 8 C 12 9.105 11.105 10 10 10 C 8.895 10 8 9.105 8 8 C 8 6.895 8.895 6 10 6 Z'
-                  fill='#FFF'
-                ></path>
-                <path
-                  d='M 5 21 C 6.105 21 7 21.895 7 23 C 7 24.105 6.105 25 5 25 C 3.895 25 3 24.105 3 23 C 3 21.895 3.895 21 5 21 Z'
-                  fill='#FFF'
-                ></path>
-                <path
-                  d='M 10 16 C 11.105 16 12 16.895 12 18 C 12 19.105 11.105 20 10 20 C 8.895 20 8 19.105 8 18 C 8 16.895 8.895 16 10 16 Z'
-                  fill='#FFF'
-                ></path>
-              </svg>
-            </motion.div>
-          </div>
-        </div>
-        {WORK_DATA.map((data) => (
-          <WorkList key={data.year} {...data} />
-        ))}
-      </main>
+          {WORK_DATA.map((data) => (
+            <WorkList key={data.year} {...data} />
+          ))}
+        </main>
 
-      <Footer scaleX={scaleX} />
-    </div>
+        <Footer scaleX={scaleX} />
+      </div>
+    </>
   );
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as TwinklingStars } from "./TwinklingStars";
 export { default as Footer } from "./Footer";
 export { default as Scene1 } from "./Scene1";
 export { default as MagneticFramer } from "./MagneticFramer";
+export { default as Preloader } from "./Preloader";
